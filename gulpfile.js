06be48da0e914be5172b5a4154b5b03093c84242
liliao <1311042097@qq.com>
2017-05-08 10:43:57 +0800

    111

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var bs = require('browser-sync')
+gulp.task('less',function(){
+    gulp.src("src/less/*.less")
+    .pipe(less())
+    .pipe(gulp.dest('dist/css'));
+})
+gulp.task('Server',['less'],function(){
+    bs.init({
+        server:{
+            baseDir:"./",
+            index:"index.html"
+        }
+    });
+    gulp.watch("src/less/*.less",['less']).on('change',bs.reload); 
+    gulp.watch("*.html").on('change',bs.reload);
+})
+gulp.task('default',['Server']);
